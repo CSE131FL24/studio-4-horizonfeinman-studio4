cf9ae8f5a1882b62bfe10dc0db8b693b284b310f
blakefeinman <blakefeinman@blakes-air.dhcp.wustl.edu>
2024-10-02 15:37:22 -0500

    studio 4

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -7,5 +7,38 @@ import edu.princeton.cs.introcs.StdDraw;
 public class Flag {
 	public static void main(String[] args) {
 		
+		StdDraw.setCanvasSize(800, 500);
+		StdDraw.setPenColor(242, 239, 41);
+		StdDraw.setPenRadius(1);
+		StdDraw.line(0, 0.5, 1, 0.5);
+		
+		StdDraw.setPenColor(50, 242, 12);
+		StdDraw.filledCircle(0.5, 0.5, 0.25);
+		
+		StdDraw.setPenColor(145, 41, 242);
+		StdDraw.setPenRadius(0.05);
+		StdDraw.line(0, 0, 1, 1);
+		StdDraw.line(0, 1, 1, 0);
+		
+		StdDraw.setPenColor(227, 34, 185);
+		StdDraw.filledCircle(0.2, 0.8, 0.04);
+		
+		StdDraw.setPenColor(227, 34, 185);
+		StdDraw.filledCircle(0.7, 0.1, 0.04);
+		
+		StdDraw.setPenColor(227, 34, 185);
+		StdDraw.filledCircle(0.8, 0.9, 0.04);
+		
+		StdDraw.setPenColor(212, 23, 23);
+		StdDraw.filledCircle(0.5, 0.6, 0.04);
+		
+		StdDraw.setPenColor(212, 23, 23);
+		StdDraw.filledCircle(0.9, 0.6, 0.04);
+		
+		StdDraw.setPenColor(212, 23, 23);
+		StdDraw.filledCircle(0.2, 0.1, 0.04);
+		
+		
+		
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -19,6 +19,68 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
-		
+
+		String shape = in.next();
+
+		int num1 = in.nextInt();
+		int num2 = in.nextInt();
+		int num3 = in.nextInt();
+
+		boolean filled = in.nextBoolean();
+
+
+		StdDraw.setPenColor(num1, num2, num3);
+
+		if (shape.equals("rectangle")) {
+
+			double x = in.nextDouble();
+			double y = in.nextDouble();
+			double halfWidth = in.nextDouble();
+			double halfHeight = in.nextDouble();
+			
+			if (filled==false) {
+				StdDraw.rectangle(x, y, halfWidth, halfHeight);
+			}
+			else {
+				StdDraw.filledRectangle(x, y, halfWidth, halfHeight);
+			}
+		}
+
+		if (shape.equals("ellipse")) {
+
+			double x = in.nextDouble();
+			double y = in.nextDouble();
+			double halfWidth = in.nextDouble();
+			double halfHeight = in.nextDouble();
+			
+			if (filled==false) {
+				StdDraw.ellipse(x, y, halfWidth, halfHeight);
+			}
+			else {
+				StdDraw.filledEllipse(x, y, halfWidth, halfHeight);
+			}
+		}
+
+		if (shape.equals("triangle")) {
+
+			double[] xNums = new double[3];
+			double[] yNums = new double[3];
+			
+			xNums[0] = in.nextDouble();
+			yNums[0] = in.nextDouble();
+			xNums[1] = in.nextDouble();
+			yNums[1] = in.nextDouble();
+			xNums[2] = in.nextDouble();
+			yNums[2] = in.nextDouble();
+			
+			if (filled==false) {
+				StdDraw.polygon(xNums, yNums);
+			}
+			else {
+				StdDraw.filledPolygon(xNums, yNums);
+			}
+		}
+
+
 	}
 }
